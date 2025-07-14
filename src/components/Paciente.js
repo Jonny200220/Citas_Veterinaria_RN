d36b2d11707c5e66a74852772e2eae7d7c11547a
Jonathan Ramirez Hdz <jonnyrmzhdz@gmail.com>
2025-07-14 00:32:51 -0600

    Anadiendo Componente de Paciente

---
+++ src/components/Paciente.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Paciente = () => {
+  return (
+    <View>
+      <Text>Pacienteeee</Text>
+    </View>
+  )
+}
+
+export default Paciente;
